acdc91278fb501d5f862167add4d2775009fafe6
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-11 00:43:34 -0300

    Clase 7: Consumiendo la FakeStore API para pintar cards
    1.Estado inicial: Dentro del componente Home, se define un estado local llamado items utilizando el hook useState. Inicialmente, items se establece en null.
    2.Efecto de efecto secundario: El componente utiliza el hook useEffect para realizar una solicitud a la API en la URL 'https://api.escuelajs.co/api/v1/products'. Esta solicitud se realiza una vez, cuando el componente se monta por primera vez (gracias al arreglo de dependencias vacío []).
    3.Renderizado: En el componente Home, se renderiza una cuadrícula de elementos utilizando el estado items. Cada elemento en el estado items se representa mediante el componente Card, al que se le pasa el dato del elemento como una prop llamada data.
    4.El componente Card acepta un objeto data como argumento, que se utiliza para renderizar los detalles del producto.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,12 +1,12 @@
-const Card = () => {
+const Card = (data) => {
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-                    Electronics
+                    {data.data.category.name}
                 </span>
                 <img 
-                    src="https://images.pexels.com/photos/3394651/pexels-photo-3394651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    src={data.data.images[0]}
                     alt="headphones"
                     className="w-full h-full object-cover rounded-lg"
                 />
@@ -15,8 +15,8 @@ const Card = () => {
                 </div>
             </figure>
                 <p className="flex justify-between">
-                    <span className="text-sm font-light">Headphones</span>
-                    <span className="text-lg font-medium">$300</span>
+                    <span className="text-sm font-light">{data.data.title}</span>
+                    <span className="text-lg font-medium">${data.data.price}</span>
                 </p>
         </div>
     )
