d10d9e9d48f776efa9773bc5d029202b9564a29c
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-14 22:51:06 -0300

    Clase 27: Filtrando categorías con JavaScript

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -26,12 +26,15 @@ export const ShoppingCartProvider = ({children}) => {
   // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
-  // Get Products · 
+  // Get products
   const [items, setItems] = useState(null)
   const [filteredItems, setFilteredItems] = useState(null)
 
-  // Get Products by title ·
+  // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
+
+  // Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
@@ -43,10 +46,34 @@ export const ShoppingCartProvider = ({children}) => {
     return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
   }
 
-  useEffect(() => { 
-    if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
-  }, [items,searchByTitle])
-console.log('filteredItems:', filteredItems);
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
+
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
 
   return (
     <ShoppingCartContext.Provider value={{
@@ -68,9 +95,9 @@ console.log('filteredItems:', filteredItems);
       setItems,
       searchByTitle,
       setSearchByTitle,
-      filteredItems
-
-      
+      filteredItems,
+      searchByCategory,
+      setSearchByCategory
     }}>
       {children}
     </ShoppingCartContext.Provider>
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -13,6 +13,11 @@ import './App.css'
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/', element: <Home /> },
+    { path: '/clothes', element: <Home /> },
+    { path: '/electronics', element: <Home /> },
+    { path: '/furnitures', element: <Home /> },
+    { path: '/toys', element: <Home /> },
+    { path: '/othes', element: <Home /> },
     { path: '/my-account', element: <MyAccount /> },
     { path: '/my-order', element: <MyOrder /> },
     { path: '/my-orders', element: <MyOrders /> },
@@ -21,7 +26,6 @@ const AppRoutes = () => {
     { path: '/sign-in', element: <SignIn /> },
     { path: '/*', element: <NotFound /> },
   ])
-
   return routes
 }
 
